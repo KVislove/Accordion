ab1057f48c3a4b19626acc409cbc2cda4843f955
Vishal Kumar Singh <vishalsingh.vs519@gmail.com>
2021-09-13 11:26:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Questions from './Components/api'
+import MyAccordion from './Components/MyAccordion';
 
 function App() {
+  const [data, setData] = useState(Questions)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main_div'>
+        <h1>Interview Questions</h1>
+        {data.map((curElem) => {
+          return <MyAccordion key={curElem.id} {...curElem} />
+        })}
+      </div>
+    </>
   );
 }
 
